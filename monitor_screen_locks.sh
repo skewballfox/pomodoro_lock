33cc1e161bb5c84fa20912199ee378ee1af47d15
Daedalus <daedalus@localhost.localdomain>
2018-08-21 18:11:28 -0500

    added license and currently debugging extended pomodoro functionality

--- monitor_screen_locks.sh
+++ monitor_screen_locks.sh
@@ -34,11 +34,11 @@ unlock_task()
   if [ "$(($duration/60))" -gt "15" ]; then
       echo "$(($duration/60))"
       counter=$((0))
-  elif [ "$counter" => "4" ]; then
+  elif [[ $counter == 4 ]]; then
       touch /tmp/extended_break_flag
       counter=$((0))
   fi
-  if [ ! -e $pom_kill_flag]; then
+  if [[ ! -e $pom_kill_flag ]]; then
     /home/daedalus/github/pomodoro_lock/pomodoro_lock.py
   fi
 }
